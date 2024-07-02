625c43afca8d827ac378342b11bfeea2ae65f6be
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-07-03 01:04:56 +0200

    world/jsdoc

--- assets/classes/world.class.js
+++ assets/classes/world.class.js
@@ -1,66 +1,104 @@
+/**
+ * That is the class World that load the game
+ */
 class World {
-  clouds = [new Cloud(), new Cloud()];
+	clouds = [new Cloud(), new Cloud()];
 	character = new Character();
 	enemies = [new Lizard(), new Lizard(), new Lizard()];
-  backgroundObjects = [
-    new BackgroundObject('assets/img/stages/1/bridge.png', 0, 80, 370),
-    new BackgroundObject('assets/img/stages/1/sea.png', 0, 0, 480),
-    new BackgroundObject('assets/img/stages/1/bamboo.png', 0, 80, 370),
-  ];
-  context;
-  canvas;
-  keyboard;
+	backgroundObjects = [
+		new BackgroundObject("assets/img/stages/1/bridge.png", 0, 80, 370),
+		new BackgroundObject("assets/img/stages/1/sea.png", 0, 0, 480),
+		new BackgroundObject("assets/img/stages/1/bamboo.png", 0, 80, 370),
+	];
+	context;
+	canvas;
+	keyboard;
 
-  constructor(canvas, keyboard){
-    this.context = canvas.getContext('2d');
-    this.canvas = canvas;
-    this.keyboard = keyboard;
-    this.draw();
-    this.setWorld();
-  }
+  /**
+   * That is the constructor that bring all elements from game.js
+   * @param {canvas} canvas - Canvas element where will be painted every img
+   * @param {class} keyboard - Class keyboard that has every movement of the player
+   */
+	constructor(canvas, keyboard) {
+		this.context = canvas.getContext("2d");
+		this.canvas = canvas;
+		this.keyboard = keyboard;
+		this.draw();
+		this.setWorld();
+	}
 
-  //This function set the world in the character, like this this one has a referenz about what happen in the world and also from the variable keyboard.
-  //This here the complete class world
-  setWorld(){
-    this.character.world = this;
-  }
+  /**
+   * This function set the world in the character, like this this one has a referenz about what
+   * happen in the world and also from the variable keyboard.
+   * This here set the complete class world
+   */
+	setWorld() {
+		this.character.world = this;
+	}
 
+  /**
+   * This function draw every element created before in the canvas
+   */
 	draw() {
-    //Clear canvas
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		//Clear canvas
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.addObjectsToMap(this.backgroundObjects);
-    this.addObjectsToMap(this.clouds);
-    this.addToMap(this.clouds[1]);
-    this.addToMap(this.character);
-    this.addObjectsToMap(this.enemies);
+		this.addObjectsToMap(this.backgroundObjects);
+		this.addObjectsToMap(this.clouds);
+		this.addToMap(this.clouds[1]);
+		this.addToMap(this.character);
+		this.addObjectsToMap(this.enemies);
 
-    //Draw will be alwys loaded
-    let self = this;
-    requestAnimationFrame(function () {
-      self.draw();
-    })
-  }
+		//Draw will be alwys loaded
+		let self = this;
+		requestAnimationFrame(function () {
+			self.draw();
+		});
+	}
 
-  addObjectsToMap(objects){
-    objects.forEach(ob => {
-      this.addToMap(ob);
-    });
-  }
+  /**
+   * This function add all elements in the array a method to be add in the canvas
+   * @param {*} objects - object array to add in the canvas
+   */
+	addObjectsToMap(objects) {
+		objects.forEach((ob) => {
+			this.addToMap(ob);
+		});
+	}
 
-  //That function drawImage is a method of getContext that allow to paint something in the canvas element. It required img path, x and y.
-  //drawImage(image, dx, dy, dWidth, dHeight)
-  addToMap(mo){
-    if(mo.otherDirection){
-      this.context.save();
-      this.context.translate(mo.width, 0);
-      this.context.scale(-1, 1);
-      mo.x = mo.x *-1;
-    }
-    this.context.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
-    if(mo.otherDirection){
-      mo.x = mo.x *-1;
-      this.context.restore();
-    }
-  }
+  /**
+   * That function drawImage is a method of getContext that allow to paint something in the canvas *element. It required img path, x and y.
+   * drawImage(image, dx, dy, dWidth, dHeight)
+   * @param {*} mo - That is the object that it will be change of posotion/Mirrow effet
+   */
+	addToMap(mo) {
+		if (mo.otherDirection) {
+			this.flipImage(mo);
+		}
+		this.context.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+		if (mo.otherDirection) {
+			this.flipImageBack(mo);
+		}
+	}
+
+  /**
+   * This function saves the canvas before make mirrow of the canvas, because with restore later, * like this it could be like before the mirrow effect.
+   * @param {*} mo - That is the object that it will be change of posotion/Mirrow effet
+   */
+	flipImage(mo) {
+		this.context.save();
+    //Mirrow of the object.
+		this.context.translate(mo.width, 0);
+		this.context.scale(-1, 1);
+		mo.x = mo.x * -1;
+	}
+
+  /**
+   * This function restores the mirrow effect of the canvas
+   * @param {object} mo - That is the object that it will be change of posotion/Mirrow effet
+   */
+	flipImageBack(mo) {
+		mo.x = mo.x * -1;
+		this.context.restore();
+	}
 }
