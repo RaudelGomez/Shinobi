e751c4e2765819bf4c066f8d1e8fc770bd3dbc32
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-07-10 12:28:37 +0200

    screen/buton colors

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,8 @@ let btnPlayPause = document.getElementById('btn-play-pause');
 let musicGame = new Audio('assets/audio/gameMusic.mp3');
 let isMusicOn = false;
 let screenGame = document.getElementById('screen-game');
+let figureFullScreen = document.getElementById('figure-fullScreen');
+let figureCloseFullScreen = document.getElementById('figure-close-fullScreen');
 	
 function init() {
   canvas = document.getElementById('canvas');
@@ -23,10 +25,10 @@ function playPauseMusic() {
   isMusicOn = !isMusicOn;
   if(isMusicOn){
     musicGame.play();
-    btnPlayPause.textContent = 'pause';
+    btnPlayPause.innerHTML = '<svg class="img-button cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>';
   }else{
     musicGame.pause();
-    btnPlayPause.textContent = 'play';
+    btnPlayPause.innerHTML = '<svg class="img-button cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>';
   }
 }
 
@@ -75,6 +77,23 @@ function disablePanelMusic() {
 function stopPropagation(event) {
   event.stopPropagation();
 }
+
+function showCloseFullScrenIcon() {
+  figureCloseFullScreen.classList.remove('d-none');
+  figureFullScreen.classList.add('d-none');
+}
+
+function showFullScrenIcon() {
+  figureFullScreen.classList.remove('d-none');
+  figureCloseFullScreen.classList.add('d-none');
+}
+
+window.addEventListener('keyup', (e)=>{
+  console.log(e);
+  if(e.keyCode == 27){
+    showFullScrenIcon();
+  }
+})
 
 
 // window.addEventListener('keydown', (e)=>{
